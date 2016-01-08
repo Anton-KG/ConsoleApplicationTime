e0a8a0963df666a73662c9e31c0b5e91c24b8edd
Anton-G <gontar.anton@gmail.com>
2016-01-08 18:18:00 +0200

    Add messaging bundle resource files

--- src/main/java/com/nocompany/MyApp.java
+++ src/main/java/com/nocompany/MyApp.java
@@ -1,5 +1,14 @@
 package com.nocompany;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MyApp {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(MyApp.class);
+        logger.info("Hello worlds");
+
+    }
 }
